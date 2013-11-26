7faff8f8957381af4884e164f30352884facf557
AlexBillerey <alex_billerey@hotmail.com>
2013-11-26 13:59:04 +0000

    code tweaks

--- space/js/main.js
+++ space/js/main.js
@@ -1,1 +1,13 @@
+function addDiv() {
+    var cp = document.createElement('div');
+    cp.id = 'controlPanel';
+    cp.title = "Control Panel";
+    cp.className = 'boo';
+    $('body').prepend(cp);
+    var cpheader = document.createElement('p');
+    var cp_txt = document.createTextNode("Control Panel");
+    $(cp).prepend(cpheader);
+    $(cpheader).prepend(cp_txt);
 
+}
+$(addDiv);
