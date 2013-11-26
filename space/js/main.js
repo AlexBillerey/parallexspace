367fb484572b311252c248c900525f201db24347
AlexBillerey <alex_billerey@hotmail.com>
2013-11-26 14:51:06 +0000

    code tweaks

--- space/js/main.js
+++ space/js/main.js
@@ -1,4 +1,4 @@
-function addDiv() {
+function init() {
     var cp = document.createElement('div');
     cp.id = 'controlPanel';
     cp.title = "Control Panel";
@@ -10,4 +10,40 @@ function addDiv() {
     $(cpheader).prepend(cp_txt);
 
 }
-$(addDiv);
+$(init);
+$(document).ready(function(){
+    $('#controlPanel').click(function(){
+        //alert('boo');
+        var pfx = ["webkit", "moz", "ms", "o", ""];
+        function RunPrefixMethod(obj, method) {
+
+            var p = 0, m, t;
+            while (p < pfx.length && !obj[m]) {
+                m = method;
+                if (pfx[p] == "") {
+                    m = m.substr(0,1).toLowerCase() + m.substr(1);
+                }
+                m = pfx[p] + m;
+                t = typeof obj[m];
+                if (t != "undefined") {
+                    pfx = [pfx[p]];
+                    return (t == "function" ? obj[m]() : obj[m]);
+                }
+                p++;
+            }
+
+        }
+        var e = document.getElementById("controlPanel");
+
+        e.onclick = function() {
+
+            if (RunPrefixMethod(document, "FullScreen") || RunPrefixMethod(document, "IsFullScreen")) {
+                RunPrefixMethod(document, "CancelFullScreen");
+            }
+            else {
+                RunPrefixMethod(e, "RequestFullScreen");
+            }
+
+        }
+    });
+})
