db30a8018788c2b8b99814fe13c645135d61f453
AlexBillerey <alex_billerey@hotmail.com>
2013-11-27 01:48:24 +0000

    code tweaks

--- space/js/main.js
+++ space/js/main.js
@@ -1,10 +1,8 @@
 function init() {
     var cp = document.createElement('div');
-    cp.id = 'controlPanel';
-    cp.title = "Control Panel";
-    cp.className = 'boo';
     $('body').prepend(cp);
     var cpheader = document.createElement('p');
+    cpheader.id = 'controlPanel';
     var cp_txt = document.createTextNode("Control Panel");
     $(cp).prepend(cpheader);
     $(cpheader).prepend(cp_txt);
